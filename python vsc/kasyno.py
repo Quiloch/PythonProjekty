30eb03c9c4a1ff507e6d9b2f9c3f7d4f9737c1bb
Quiloch <Quiloch@protonmail.com>
2026-03-26 13:28:02 +0100

    Dodanie JSON (obsługa zapisu gry)

--- python vsc/kasyno.py
+++ python vsc/kasyno.py
@@ -1,66 +1,19 @@
 import random
-'''
-saldo = 100
-mnozniki = {"łatwy" : 2, "trudny": 5}
-historia_gier = []
+import json
 
-def pobierz_stawke(aktualne_saldo):
-    while True:
-        try:
-            stawka = int(input(f"Podaj stawke, za jaka chcesz zagrac\n"))
-            if 0 < stawka <= aktualne_saldo:                    return stawka
-            else:
-                print("Podana stawka jest nieprawidlowa, sprobuj ponownie")
-        except ValueError:
-            print("Podana wartosc nie jest liczba")
-
-
-       
-while saldo > 0:
-    print("saldo: ", saldo)
-    try:
-        poziom = int(input(f"Na jakim poziomie chcesz zagrać? (łatwy - 1/trudny - 2)\n"))
-        stawka = pobierz_stawke(saldo)
-        saldo -= stawka
-    except ValueError:
-        print("Podana wartosc nie jest liczba")
-        continue
-
-    if poziom == 1:
-        wylosowana_liczba = random.randint(1,3)
-        try:
-            typowanie_gracza = int(input("Jaka liczba (1-3) została wylosowana?\n"))
-        except ValueError:
-            print("Podana wartosc nie jest liczba") 
-        if wylosowana_liczba == typowanie_gracza:
-            stawka *= mnozniki["łatwy"]
-            saldo += stawka
-            historia_gier.append("wygrana")
-            print("wylosowana liczba to: ", wylosowana_liczba)
-        else:
-            print("Tym razem sie nie udalo, wylosowana liczba to: ", wylosowana_liczba)
-            historia_gier.append("Przegrana")
-
-    elif poziom == 2:
-        wylosowana_liczba = random.randint(1,10)
-        try:
-            typowanie_gracza = int(input(f"Jaka liczba (1-10) została wylosowana\n"))
-        except ValueError:
-            print("Podana wartosc nie jest liczba")
-        if wylosowana_liczba == typowanie_gracza:
-            stawka *= mnozniki["trudny"]
-            saldo += stawka
-            historia_gier.append("wygrana")
-            print("wylosowana liczba to: ", wylosowana_liczba)
-        else:
-            print("Tym razem sie nie udalo, wylosowana liczba to: ", wylosowana_liczba)
-            historia_gier.append("Przegrana")
-'''
 class Kasyno:
     def __init__(self):
-        self.saldo = 100
+        try:
+            with open("zapis_gry.json", "r") as plik:
+                wczytany_stan = json.load(plik)
+                self.saldo = wczytany_stan["saldo"]
+                self.historia_gier = wczytany_stan["historia_gier"]
+        except (FileNotFoundError, json.JSONDecodeError):
+            print("Nie znaleziono pliku z zapisem gry lub plik jest uszkodzony, ustawiam saldo na domyślną wartość 100zł!")
+            self.saldo = 100
+            self.historia_gier = []
         self.mnozniki = {"łatwy" : 2, "trudny": 5}
-        self.historia_gier = []
+
 
     def pobierz_stawke(self):
         while True:
@@ -81,7 +34,7 @@ class Kasyno:
     def zagraj(self, poziom, stawka):
         self.saldo -= stawka
 
-        if self.poziom == 1:
+        if poziom == 1:
             wylosowana_liczba = random.randint(1,3)
             try:
                 typowanie_gracza = int(input("Jaka liczba (1-3) została wylosowana?\n"))
@@ -96,7 +49,7 @@ class Kasyno:
             except ValueError:
                 print("Podana wartosc nie jest liczba")
             
-        elif self.poziom == 2:
+        elif poziom == 2:
             wylosowana_liczba = random.randint(1,10)
             try:
                 typowanie_gracza = int(input(f"Jaka liczba (1-10) została wylosowana\n"))
@@ -110,8 +63,16 @@ class Kasyno:
                     self.historia_gier.append("Przegrana")
             except ValueError:
                 print("Podana wartosc nie jest liczba")
-            
 
+        self.zapisz_gre()
+            
+    def zapisz_gre(self):
+        stan_gry = {
+            "saldo": self.saldo,
+            "historia_gier": self.historia_gier
+        }
+        with open("zapis_gry.json", "w") as plik:
+            json.dump(stan_gry, plik)
 
 moje_kasyno = Kasyno()
 
