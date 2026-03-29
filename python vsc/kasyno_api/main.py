013344bffc1988d12b65216529965f5f8bdcba47
Quiloch <Quiloch@protonmail.com>
2026-03-29 17:55:54 +0200

    Import modeli, silnika bazy danych i stworzenie tabel na podstawie modelu

--- python vsc/kasyno_api/main.py
+++ python vsc/kasyno_api/main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
 from routers import router #import router z pliku routers.py
+import models #import modeli z pliku models.py
+from database import engine #import silnika bazy danych z pliku database.py
+
+#Stworzenie tabel w bazie danych na podstawie modeli
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI() #tworzenie instancji aplikacji FastAPI
 
