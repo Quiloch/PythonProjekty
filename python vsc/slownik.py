f6cd7613eae9685be685a917ca5dc879b96e05ea
Quiloch <Quiloch@protonmail.com>
2026-04-02 18:50:04 +0200

    Usunięcie nieużywanych plików.

--- python vsc/slownik.py
+++
@@ -1,4 +0,0 @@
-oceny_ucznia = {"Matematyka" : 4, "Polski": 5, "Fizyka":3}
-
-for przedmiot, ocena in oceny_ucznia.items():
-    print(f"Z przedmiotu {przedmiot} ocena wynosi {ocena}")
--- python vsc/ulubionedanie.py
+++
@@ -1,8 +0,0 @@
-
-with open("jedzenie.txt", "a") as file:
-    file.write(input("Jakie jest Twoje ulubione danie?\n") + "\n")
-
-with open("jedzenie.txt", "r") as file:
-    zawartosc = file.read()
-    print("Twoje ulubione dania to:")
-    print(zawartosc)
